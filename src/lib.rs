fea61fb259c25ffb152ece21ce6134eeb0ecf688
rwblickhan <rwblickhan@gmail.com>
2018-02-09 12:37:42 -0800

    Add constructor and basic unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,1 @@
 pub mod vec3;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
--- src/vec3.rs
+++ src/vec3.rs
@@ -1,19 +1,28 @@
-use std::ops::{Add, AddAssign};
+use std::ops::{Add};
 
+#[derive(PartialEq, Debug)]
 pub struct Vec3<T> {
     pub x: T,
     pub y: T,
-    pub z: T
+    pub z: T,
 }
 
-impl<T: Add<Output=T>> Add for Vec3<T> {
+impl<T> Vec3<T> {
+    pub fn new(x: T, y: T, z: T) -> Vec3<T> {
+        Vec3 {x, y, z}
+    }
+}
+
+// TODO implement copy trait
+
+impl<T: Add<Output = T>> Add for Vec3<T> {
     type Output = Vec3<T>;
 
     fn add(self, rhs: Vec3<T>) -> Vec3<T> {
         Vec3 {
             x: self.x + rhs.x,
             y: self.y + rhs.y,
-            z: self.z + rhs.z
+            z: self.z + rhs.z,
         }
     }
 }
@@ -28,3 +37,24 @@ impl<T: Add<Output=T>> Add for Vec3<T> {
 //        }
 //    }
 //}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn equality() {
+        let a = Vec3::new(1,2,3);
+        let b = Vec3::new(1,2,3);
+        assert_eq!(a,b);
+        let c = Vec3::new(3,2,1);
+        assert_ne!(a,c);
+    }
+
+    #[test]
+    fn add() {
+        let a = Vec3::new(1,1,1);
+        let b = Vec3::new(1,1,1);
+        let c = Vec3::new(2,2,2);
+        assert_eq!(a+b, c);
+    }
+}
