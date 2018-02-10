d90314b502b60b8e6b277c827d2b172349127b2c
rwblickhan <rwblickhan@gmail.com>
2018-02-09 18:05:40 -0800

    Overload mul_assign and div_assign for vec3

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,4 +1,4 @@
-use std::ops::{Add, AddAssign, Div, Mul};
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign};
 
 #[derive(Copy, Clone, PartialEq, Debug)]
 pub struct Vec3<T> {
@@ -60,6 +60,16 @@ impl<T: Mul<Output = T> + Copy> Mul<T> for Vec3<T> {
     }
 }
 
+impl<T: Mul<Output = T> + Copy> MulAssign<T> for Vec3<T> {
+    fn mul_assign(&mut self, rhs: T) {
+        *self = Vec3 {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+        }
+    }
+}
+
 impl<T: Div<Output = T> + Copy> Div<T> for Vec3<T> {
     type Output = Vec3<T>;
 
@@ -67,6 +77,16 @@ impl<T: Div<Output = T> + Copy> Div<T> for Vec3<T> {
     fn div(self, rhs: T) -> Vec3<T> {
         // TODO deal with divide-by-zero/NaN
         Vec3 {
+            x: self.x / rhs,
+            y: self.y / rhs,
+            z: self.z / rhs,
+        }
+    }
+}
+
+impl<T: Div<Output = T> + Copy> DivAssign<T> for Vec3<T> {
+    fn div_assign(&mut self, rhs: T) {
+        *self = Vec3 {
             x: self.x / rhs,
             y: self.y / rhs,
             z: self.z / rhs,
@@ -118,10 +138,26 @@ mod tests {
     }
 
     #[test]
+    fn mul_scalar_assign_i32() {
+        let mut a = Vec3::new(1, 1, 1);
+        a *= 2;
+        let b = Vec3::new(2, 2, 2);
+        assert_eq!(a, b);
+    }
+
+    #[test]
     fn div_scalar_i32() {
         let a = Vec3::new(1, 1, 1);
         let b = Vec3::new(2, 2, 2);
         assert_eq!(b / 2, a);
+    }
+
+    #[test]
+    fn div_scalar_assign_i32() {
+        let mut a = Vec3::new(2, 2, 2);
+        a /= 2;
+        let b = Vec3::new(1, 1, 1);
+        assert_eq!(a, b);
     }
 
     #[test]
@@ -165,9 +201,25 @@ mod tests {
     }
 
     #[test]
+    fn mul_scalar_assign_f64() {
+        let mut a = Vec3::new(1.0, 1.0, 1.0);
+        a *= 2.0;
+        let b = Vec3::new(2.0, 2.0, 2.0);
+        assert_eq!(a, b);
+    }
+
+    #[test]
     fn div_scalar_f64() {
         let a = Vec3::new(1.0, 1.0, 1.0);
         let b = Vec3::new(2.0, 2.0, 2.0);
         assert_eq!(b / 2.0, a);
+    }
+
+    #[test]
+    fn div_scalar_assign_f64() {
+        let mut a = Vec3::new(2.0, 2.0, 2.0);
+        a /= 2.0;
+        let b = Vec3::new(1.0, 1.0, 1.0);
+        assert_eq!(a, b);
     }
 }
